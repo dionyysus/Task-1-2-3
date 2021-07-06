3dee42d1794613d7fa0b2babc817004b54f7dd5a
dionyysus <gizemcsk1@gmail.com>
2021-07-06 12:00:44 +0300

    task1 completed

--- src/Entity/Main.java
+++ src/Entity/Main.java
@@ -9,10 +9,23 @@ public class Main {
 		
 		System.out.println("Please enter the your ID");
 		String identity = sc.nextLine();
-
+		
 		SumNumbers sumNumbers = new SumNumbers(identity);
 	
 		System.out.println("Sum of ID: " +sumNumbers.ConvertToInt(identity));
+		
+		
+		// 2. Method
+		
+		int sum = 0;
+		
+		for(int i = 0; i< 11; i++) {
+			System.out.println("Please enter the your characters of ID "+i);
+			int id = sc.nextInt();
+			sum += id;
+		}
+		
+		System.out.println("Sum of ID: " +sum);
 		
 
 	}
